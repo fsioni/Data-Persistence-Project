59bca7270a72bf6dfb5167d3f8afcf77ab552b07
fsioni <fares.sioni@gmail.com>
2022-03-10 12:24:15 +0100

    Adding : saving between scenes : player name

---
+++ Assets/Scripts/MainDataManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDataManager : MonoBehaviour
+{
+    public string playerName;
+    
+    public static MainDataManager Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField playerNameInput;
+    [SerializeField] private GameObject mustEnterNameText;
+    
+    public void StartButton()
+    {
+        if (playerNameInput.text == "")
+        {
+            mustEnterNameText.SetActive(true);
+            return;
+        }
+        MainDataManager.Instance.playerName = playerNameInput.text;
+        SceneManager.LoadScene(0);
+    }
+}
